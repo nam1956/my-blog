06106204b04ae99d69c5fb394cc66b9fa9a7a52f
nam1956 <nam1956y@gmail.com>
2026-02-20 22:41:39 +0900

    추가파일data,js

---
+++ data.js
@@ -0,0 +1,8 @@
+// data.js
+const photoData = [
+    { filename: "55563456.jpg", date: "2023-12-20", title: "북한산 비봉 코스", tags: "등반, 겨울, 서울, snow", category: "hiking" },
+    { filename: "146132676.jpg", date: "2023-08-15", title: "환갑 잔치", tags: "가족, 잔치, 한정식, food", category: "family" },
+    { filename: "177331263.jpg", date: "2023-08-16", title: "설악산", tags: "바위사랑, 동구회", category: "friend" },
+    { filename: "811274546.jpg", date: "1980-08-08", title: "어린 시절", tags: "나나", category: "hiking" },
+    // ... 나머지 50개 데이터도 이런 식으로 추가하면 됩니다.
+];
